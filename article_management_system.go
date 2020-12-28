3ebb6cd2b28e739fae8793bf0cf8d2320a91db82
aanujj <31202716+aanujj@users.noreply.github.com>
2020-12-29 01:15:35 +0530

    Create article_management_system.go

---
+++ article_management_system.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "test title", Desc: "test descprition", Content: "helo world"},
+	}
+	fmt.Println("Endpoint : Hit all article Endpoint ")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func main() {
+	handleRequests()
+}
